68f4483bd57ca7153219fefe4cb76f07d8459d94
Devpatel90 <devsojitra02@gmail.com>
2024-12-12 19:26:26 +0530

    File Uploaded

---
+++ Practical Exam/1.js
@@ -0,0 +1,26 @@
+let q1= [];
+let q2= [];
+
+function push(a)
+{
+        q2.push(a)
+        while(q1.length > 0)
+        {
+            q2.push(q1.shift())
+        }
+
+        [q1,q2] = [q2,q1]
+
+}
+
+function pop(){
+    return q1.shift()
+}
+
+push(10)
+push(20)
+push(30)
+push(40)
+push(50)
+
+console.log(pop());
